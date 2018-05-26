dac5402e658a7d0fa78ae6491b8a7487b7e3d07a
Mario Schulz <Mario.Schulzqdeutsche-boerse.com>
2018-05-27 00:18:28 +0200

    Use stcgal as prommer

--- Program HEX and C file/upload.bat
+++ Program HEX and C file/upload.bat
@@ -1,1 +1,3 @@
-C:\Python36\Scripts\stcgal.exe -P stc12 -p com7: 888n.ihx disp.hex
+C:\Python36\Scripts\stcgal.exe -P stc12 -p com7: 888n.ihx
+rem disp.hex (write the user eeprom area)
+rem -o eeprom_erase_enabled=true (erase the user eeprom area)
