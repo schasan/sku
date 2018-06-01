a86284ae858fa88001bb9c91c9dae558fe94537d
Mario Schulz <Mario.Schulzqdeutsche-boerse.com>
2018-06-01 19:55:36 +0200

    Back to ATEN

--- Program HEX and C file/upload.bat
+++ Program HEX and C file/upload.bat
@@ -1,3 +1,3 @@
-C:\Python36\Scripts\stcgal.exe -P stc12 -p com6: -o eeprom_erase_enabled=false 888n.ihx
+C:\Python36\Scripts\stcgal.exe -P stc12 -p com7: -o eeprom_erase_enabled=false 888n.ihx
 rem disp.hex (write the user eeprom area)
 rem -o eeprom_erase_enabled=true (erase the user eeprom area)
